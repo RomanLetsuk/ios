acf094a78a25bbc21f52f6e0f9300d522c60ac1e
RomanLetsuk <romaletsuk@gmail.com>
2019-03-21 12:04:25 +0300

    implemented diagonal difference method

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -2,9 +2,23 @@
 
 @implementation Diagonal
 
-// Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    NSUInteger arrayCount = [array count];
+    NSMutableArray *updatedArray = [[NSMutableArray alloc] initWithCapacity:arrayCount];
+    
+    [array enumerateObjectsUsingBlock:^(NSString *string, NSUInteger index, BOOL *stop) {
+        [updatedArray addObject:[[NSArray alloc] initWithArray:[string componentsSeparatedByString:@" "]]];
+    }];
+    
+    int firstDiagonal = 0;
+    int secondDiagonal = 0;
+    
+    for (int i = 0; i < arrayCount; i += 1) {
+        firstDiagonal += [(NSNumber *)updatedArray[i][i] intValue];
+        secondDiagonal += [(NSNumber *)updatedArray[arrayCount - i - 1][i] intValue];
+    }
+    
+    return [NSNumber numberWithInt:abs(firstDiagonal - secondDiagonal)];
 }
 
 @end
