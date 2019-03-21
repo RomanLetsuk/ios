df6e6e661d305b4ffa8fceeb05b70875342e8220
RomanLetsuk <romaletsuk@gmail.com>
2019-03-21 13:54:05 +0300

    implemented encryption method

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -2,9 +2,44 @@
 
 @implementation Encryption
 
-// Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    NSString *stringWithoutSpaces = [[string componentsSeparatedByString:@" "] componentsJoinedByString:@""];
+    
+    double tempLength = sqrt([stringWithoutSpaces length]);
+    int rows = (int)floor(tempLength);
+    int columns = (int)ceil(tempLength);
+    
+    if (rows * columns < [stringWithoutSpaces length]) {
+        rows += 1;
+    }
+    
+    NSMutableArray *encryptionMatrix = [[NSMutableArray alloc] initWithCapacity:rows];
+    
+    for (int i = 0; i < rows; i += 1) {
+        NSUInteger rangeLength = columns;
+
+        if (rows - 1 == i) {
+            rangeLength = [stringWithoutSpaces length] - columns *  i;
+        }
+        
+        encryptionMatrix[i] = [[NSString alloc] initWithString:[stringWithoutSpaces substringWithRange:NSMakeRange(i * columns, rangeLength)]];
+    }
+    
+    NSMutableString *result = [[NSMutableString alloc] initWithCapacity: [stringWithoutSpaces length] + rows];
+    
+    for (NSUInteger columnIndex = 0; columnIndex < columns; columnIndex += 1) {
+        for (NSUInteger rowIndex = 0; rowIndex < rows; rowIndex += 1) {
+            NSString *row = encryptionMatrix[rowIndex];
+            
+            if ([row length] > columnIndex) {
+                [result appendString:[NSString stringWithFormat:@"%c", [row characterAtIndex:columnIndex]]];
+            }
+        }
+        
+        [result appendString:@" "];
+    }
+    
+    return [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
 
 @end
