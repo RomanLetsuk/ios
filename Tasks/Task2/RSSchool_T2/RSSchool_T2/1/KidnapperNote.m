edccfa4865439a0320d32f2a131a0f776f0a0ce7
RomanLetsuk <romaletsuk@gmail.com>
2019-03-29 14:42:45 +0300

    implemented  method for checking magazine with notes

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,27 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    NSMutableArray *magazineWords = [[NSMutableArray alloc] initWithArray:[[magaine lowercaseString] componentsSeparatedByString:@" "]];
+    NSArray *noteWords = [[NSArray alloc] initWithArray:[[note lowercaseString] componentsSeparatedByString:@" "]];
+    BOOL result = true;
+    
+    for (NSString *item in noteWords) {
+        NSUInteger index = [magazineWords indexOfObject:item];
+        
+        if (index == NSNotFound) {
+            result = false;
+            break;
+        }
+        
+        [magazineWords removeObjectAtIndex:index];
+    }
+    
+    [magazineWords release];
+    [noteWords release];
+    
+    return result;
+}
+
 @end
