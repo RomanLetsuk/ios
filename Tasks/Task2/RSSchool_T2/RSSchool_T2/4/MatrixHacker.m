c79083806495649cc183cfa4e7ab5dd757cc724b
RomanLetsuk <romaletsuk@gmail.com>
2019-04-02 16:38:55 +0300

    completed matrix  hacker  methods

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -1,5 +1,35 @@
 #import "MatrixHacker.h"
 // your code here
+@interface MatrixHacker()
+@property (nonatomic, copy) id<Character> (^block)(NSString *name);
+@end
+
 @implementation MatrixHacker
-// your code here
+
+@synthesize block = _block;
+
+- (id<Character> (^)(NSString *))block {
+    return _block;
+}
+
+- (void)setBlock:(id<Character> (^)(NSString *))block {
+    [_block release];
+    _block = [block copy];
+}
+
+- (void)injectCode:(id<Character> (^)(NSString *name))theBlock {
+    [self setBlock:theBlock];
+}
+
+- (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
+    NSMutableArray *array = [NSMutableArray new];
+    
+    for (NSString *name in names) {
+        [array addObject: _block(name)];
+    }
+    
+    NSArray *resultArray = [[NSArray alloc] initWithArray:array];
+    return resultArray;
+}
+
 @end
